91ede4c81137677d783f53286a5d27f5b3f6c5ba
Diego Cezarino <cezarino.diego@gmail.com>
2021-04-21 13:00:27 -0300

    Adicionada anotação @Repository

--- domain/src/main/java/com/devsuperior/repository/ProductRepository.java
+++ domain/src/main/java/com/devsuperior/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.devsuperior.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.devsuperior.entity.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
