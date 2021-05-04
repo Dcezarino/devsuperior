3f995869cfa479569918e7046326ce5088b48298
Diego Cezarino <cezarino.diego@gmail.com>
2021-05-03 21:03:42 -0300

    Modified data

--- projeto-sds3/frontend/src/components/DataTable/index.tsx
+++ projeto-sds3/frontend/src/components/DataTable/index.tsx
@@ -21,32 +21,32 @@ const DataTable = () => {
                         <td>15017.00</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
+                        <td>01/05/2021</td>
+                        <td>Ray Disney</td>
                         <td>25</td>
-                        <td>15017.00</td>
+                        <td>32</td>
+                        <td>18025.43</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>21/04/2021</td>
+                        <td>Willians Fantasy</td>
+                        <td>58</td>
+                        <td>13</td>
+                        <td>25019.35</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>18/04/2021</td>
+                        <td>Melody Star</td>
+                        <td>41</td>
+                        <td>50</td>
+                        <td>35025.12</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>04/04/2021</td>
+                        <td>Mary Katy</td>
+                        <td>53</td>
+                        <td>22</td>
+                        <td>40014.10</td>
                     </tr>
                 </tbody>
             </table>
