b03ab469842da2b6b3f28000381391f1ddd1c317
Diego Cezarino <cezarino.diego@gmail.com>
2021-09-09 23:41:32 -0300

    Group by search

--- projeto-sds4/backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
+++ projeto-sds4/backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
@@ -29,13 +29,13 @@ public class SaleController {
 	}
 
 	@GetMapping(value = "/amount-by-seller")
-	public ResponseEntity<List<SaleSumDTO>> amountGroupedBySeller() {
+	public ResponseEntity<List<SaleSumDTO>> amountGroupedBySeller(){
 		List<SaleSumDTO> list = saleService.amountGroupedBySeller();
 		return ResponseEntity.ok(list);
 	}
 
 	@GetMapping(value = "/success-by-seller")
-	public ResponseEntity<List<SaleSuccessDTO>> successGroupedBySeller() {
+	public ResponseEntity<List<SaleSuccessDTO>> successGroupedBySeller(){
 		List<SaleSuccessDTO> list = saleService.successGroupedBySeller();
 		return ResponseEntity.ok(list);
 	}
